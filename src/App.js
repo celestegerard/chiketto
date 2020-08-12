9d4df732ba3dbabc2ee252a2944232ffe7d1424b
celestegerard <celeste.gerard@gmail.com>
2020-08-12 11:39:26 -0400

    morning

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import jackson from './jackson.jpeg';
+
+
 
 class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  handlePlusClick = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  };
+
+  handleMinusClick = () => {
+    this.setState(({ count }) => ({
+      count: count - 1
+    }));
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Chiketto</h1>
+          <h1 className="App-title">Ticket</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img className="Profile" src={jackson} alt="jackson" />
+          <div className="count">
+          <button onClick={this.handleMinusClick} className="minus">-</button>
+          <div className="counter">{this.state.count}</div>
+          <button onClick={this.handlePlusClick} className="plus">+</button>
+          </div>
       </div>
     );
   }
