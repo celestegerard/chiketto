b91a2506b9d1402ae3c8a7dc2e4e04f91112c810
celestegerard <celeste.gerard@gmail.com>
2020-08-12 16:49:30 -0400

    added prizes and prize filter based on count

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import jackson from './jackson.jpeg';
+import bouncyball from './prizes/bouncyball.jpg'
+import dino from './prizes/dino.jpg'
+import jelloplay from './prizes/jelloplay.jpeg'
+import peppa from './prizes/peppabandaid.jpg'
+import chalk from './prizes/chalk.jpg'
+import fish from './prizes/fish.jpg'
+import lizards from './prizes/lizards.jpg'
 
 
 
 class App extends Component {
 
   state = {
-    count: 0
+    count: 0,
+    bouncyball: 2,
+    dino: 3,
+    peppa: 4,
+    chalk: 5,
+    lizards: 6,
+    fish: 7
   }
 
   handlePlusClick = () => {
@@ -22,6 +35,7 @@ class App extends Component {
     }));
   };
 
+
   render() {
     return (
       <div className="App">
@@ -33,6 +47,15 @@ class App extends Component {
           <button onClick={this.handleMinusClick} className="minus">-</button>
           <div className="counter">{this.state.count}</div>
           <button onClick={this.handlePlusClick} className="plus">+</button>
+          </div>
+
+          <div>
+          {this.state.bouncyball <= this.state.count ? <React.Fragment><img className="Prize" src={bouncyball} alt="bouncyball" /> <p className="price">{this.state.bouncyball}</p> </React.Fragment> : null }
+          {this.state.dino <= this.state.count ? <React.Fragment><img className="Prize" src={dino} alt="bouncyball" /><p className="price">{this.state.dino}</p></React.Fragment> : null }
+          {this.state.lizards <= this.state.count ? <React.Fragment><img className="Prize" src={lizards} alt="bouncyball" /><p className="price">{this.state.lizards}</p></React.Fragment> : null }
+{this.state.peppa <= this.state.count ? <React.Fragment><img className="Prize" src={peppa} alt="bouncyball" /><p className="price">{this.state.peppa}</p></React.Fragment> : null }
+{this.state.chalk <= this.state.count ? <React.Fragment><img className="Prize" src={chalk} alt="bouncyball" /><p className="price">{this.state.chalk}</p></React.Fragment> : null }
+{this.state.fish <= this.state.count ? <React.Fragment><img className="Prize" src={fish} alt="bouncyball" /><p className="price">{this.state.fish}</p></React.Fragment> : null }
           </div>
       </div>
     );
