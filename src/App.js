9ca5f52e4cafee2dfce9096b6ac872c04cee4a7f
celestegerard <celeste.gerard@gmail.com>
2020-08-25 18:50:28 -0400

    added prizebox page

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ handleUserClick = () => {
   })
 }
 
-handlePrizeBox = () => {
+handlePrizeBoxClick = () => {
   this.setState({
     prizeBox: !this.state.prizeBox
   })
@@ -89,6 +89,7 @@ handlePrizeBox = () => {
           <button onClick={this.handleUserClick} >Users</button>
           <button onClick={this.handlePrizeBoxClick} >Prize Box</button>
         </header>
+        {this.state.prizeBox ? <React.Fragment><div class="PrizeBoxContainerL"><img className="Prize" src={bouncyball} alt="bouncyball" /><p className="boxprice">{this.state.bouncyball}</p></div><div class="PrizeBoxContainerR"><img className="Prize" src={dino} alt="dino" /><p className="boxprice">{this.state.dino}</p></div><div class="PrizeBoxContainer"><img className="Prize" src={lizards} alt="lizards" /><p className="boxprice">{this.state.lizards}</p></div><div class="PrizeBoxContainer"><img className="Prize" src={peppa} alt="peppa" /><p className="boxprice">{this.state.peppa}</p></div><div class="PrizeBoxContainer"><img className="Prize" src={chalk} alt="chalk" /><p className="boxprice">{this.state.chalk}</p></div><div class="PrizeBoxContainer"><img className="Prize" src={fish} alt="fish" /><p className="boxprice">{this.state.fish}</p></div></React.Fragment> : null }
         {this.state.usersShow ? <React.Fragment><div className="UserTopSpace"></div><div className="MeterBlue"><img className="User" src={ellie} alt="jackson" /></div><div onClick={this.handleUserClick} className="UserMeterBlue"><img className="User" src={avatar} alt="jackson" /></div><div className="UserMeterBlue"><img className="User" src={wes} alt="jackson" /></div><div className="UserBottomSpace"></div></React.Fragment> : null}
           <div className="MeterBlue">
         <img className="Profile" src={avatar} alt="jackson" />
