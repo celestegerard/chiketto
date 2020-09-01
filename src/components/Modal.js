16b96a5b3e9b65a128b33ea78daf4c789608bf8e
celestegerard <celeste.gerard@gmail.com>
2020-09-01 19:53:20 -0400

    refined are you sure modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,9 +11,10 @@ export default class Modal extends React.Component {
       <React.Fragment>
       <div className="Modal">
       <div className="Modalmain">
-      <p >Would you like this {this.props.prize} for {this.props.price} tickets?</p>
-      <button className="ModalButton" onClick={this.props.subtractFromCount}> Yes </button>
-      <button className="ModalButton" >No</button>
+      <p className="modal-text">Buy this {this.props.prize} for {this.props.price} tickets?</p>
+      <button className="ModalButton-yes" onClick={this.props.subtractFromCount}> Yes </button>
+      <br />
+      <button className="ModalButton-no" >No</button>
       </div>
       </div>
       </React.Fragment>
