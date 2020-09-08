843b666824d9c98ef2e7b95d155783b20d316726
celestegerard <celeste.gerard@gmail.com>
2020-09-08 18:37:24 -0400

    router prepped

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+
 import jackson from './jackson.jpeg';
 import bouncyball from './prizes/bouncyball.jpg'
 import dino from './prizes/dino.jpg'
