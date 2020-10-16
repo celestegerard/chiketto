6917d101698248418251440e4e1b501bdb1b97d4
celestegerard <celeste.gerard@gmail.com>
2020-10-16 16:26:06 -0600

    rework PATCH

---
+++ build/precache-manifest.dbe341ec7b0e81cf91d3ccf79a65c7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4cbdd0f713b059f27693365d8220c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81121036573cf33e6545",
+    "url": "/static/css/main.84ff4b98.chunk.css"
+  },
+  {
+    "revision": "91a871eb13420e6a74a6",
+    "url": "/static/js/2.84e438ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.84e438ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81121036573cf33e6545",
+    "url": "/static/js/main.14e5cd69.chunk.js"
+  },
+  {
+    "revision": "1190f226ae475544e8f1",
+    "url": "/static/js/runtime-main.9081f71b.js"
+  }
+]);
